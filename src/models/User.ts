c9f274f9c2eecb49e1c2bd6c00df9ab67f22d538
pouya dehghani <pouyadh.dev@gmail.com>
2023-05-05 14:10:35 +0330

    add User Model

---
+++ src/models/User.ts
@@ -0,0 +1,31 @@
+import mongoose, { Document, Schema } from 'mongoose';
+import bcrypt from 'bcrypt';
+
+export interface IUser {
+   username: string;
+   email: string;
+   name: string;
+   avatarUrl: string;
+   hashedPassword: string;
+   refreshToken?: string;
+}
+
+export interface IUserModel extends IUser, Document {}
+
+const UserSchema: Schema = new Schema(
+   {
+      username: { type: String, required: true, trim: true, lowercase: true, unique: true },
+      email: { type: String, required: true, trim: true, lowercase: true, unique: true },
+      name: { type: String, required: true },
+      avatarUrl: { type: String, required: true },
+      hashedPassword: { type: String, required: true },
+      refreshToken: { type: String }
+   },
+   { timestamps: true, versionKey: false }
+);
+
+UserSchema.methods.comparePassword = function (password: string) {
+   return bcrypt.compareSync(password, this.hashedPassword);
+};
+
+export default mongoose.model<IUserModel>('User', UserSchema);
