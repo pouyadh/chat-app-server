9876bde1211bc4445c28a10e87204427e9128971
pouya dehghani <pouyadh.dev@gmail.com>
2023-05-05 14:10:03 +0330

    add auth.ts middleware

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,20 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+import { config } from '../config/config';
+import { AppRequest } from '../types';
+
+const auth =
+   (required: boolean = false) =>
+   (req: AppRequest, res: Response, next: NextFunction) => {
+      const { accessToken } = req.cookies;
+      if (!accessToken) return required ? res.sendStatus(401) : next();
+      const [schema, token] = accessToken.split(' ');
+      if (schema !== 'Bearer' || !token) return required ? res.sendStatus(401) : next();
+      try {
+         req.user = jwt.verify(token, config.token.access.secret);
+         next();
+      } catch (error) {
+         return required ? res.sendStatus(403) : next();
+      }
+   };
+export default auth;
